cee0268d3359620d03f9c4f0b41e2f0581391aeb
sand <sand192356@gmIL.COM>
2025-04-25 21:44:36 +0800

    新增pm25

--- app.py
+++ app.py
@@ -1,17 +1,67 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from datetime import datetime
 import pandas as pd
 import pymysql
+from pm25 import get_pm25_data_from_mysql
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
+    colums,datas= get_pm25_data_from_mysql()
+    return render_template("index.html" , colums=colums,datas=datas)
+
+@app.route("/books")
+def books_index():
+    books=[
+    {
+        "name":"Python book",
+        "price":299,
+        "image_url":"https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348"
+    },
+    {
+
+        "name":"Java book",
+        "price":399,
+        "image_url":"https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348"
+    },
+    {
+        "name":"C# book",
+        "price":499,
+        "image_url":"https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348"
+    },
+    ]
+
+    #books = []
+
+    if books:
+        for book in books:
+            print(book["name"])
+            print(book["price"])
+            print(book["image_url"])
+        else:
+            print("販售完畢，目前無書籍!")
+
     #return f"<h1>Hello world!</h1><br>{datetime.now()}"
-    username = "snad"
+    username = "sand"
     nowtime = datetime.now().strftime("%Y-%m-%d")
     print(username,nowtime)
-    return render_template("index.html",name=username ,now=nowtime)
+    return render_template("books.html",name=username ,now=nowtime, books=books)
+
+@app.route("/bmi")
+def get_bmi():
+    # args =>GET
+    height=eval(request.args.get("height"))
+    weight=eval(request.args.get("weight"))
+
+    bmi=round(weight / (height / 100 ) **2,2)
+
+    
+    #return {"height": height, "weight":weight ,"bmi":bmi}
+    #return render_template("bmi.html", bmi=bmi ,height=height,weight=weight)
+    return render_template("bmi.html",**locals())
+
+
 
 @app.route("/pm25-data")
 def get_pm25_data():
@@ -20,5 +70,7 @@ def get_pm25_data():
     df["datacreationdate"]=pd.to_datetime(df["datacreationdate"])
     df1=df.dropna()
     return df1
-app.run(debug=True)
+
+if __name__=="__main__":
+    app.run(debug=True)
 
